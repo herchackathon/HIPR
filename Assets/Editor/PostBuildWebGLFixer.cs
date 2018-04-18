cf035dbc1a8cec5f9fec0067aa8de9d5acd54ad1
Emanuele <m4nu.91@gmail.com>
2018-04-18 23:41:35 +0200

    PostBuild event to fix the warning of WebGL on mobile

---
+++ Assets/Editor/PostBuildWebGLFixer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class PostBuildWebGLFixer : MonoBehaviour
+{
+    [PostProcessBuild]
+    public static void OnPostProcessWebGLBuild(BuildTarget target, string path)
+    {
+        if (target == BuildTarget.WebGL)
+        {
+            DisableWebGLWarningOnMobile(path);
+        }
+    }
+
+    private static void DisableWebGLWarningOnMobile(string path)
+    {
+        var loaderPath = Path.Combine(Path.Combine(path, "Build"), "UnityLoader.js");
+        var content = File.ReadAllText(loaderPath);
+
+        var actualContentRegex = @"compatibilityCheck:function\(e,t,r\)\{.+,Blobs:\{\},loadCode";
+        var contentToReplaceRegex = "compatibilityCheck:function(e,t,r){t()},Blobs:{},loadCode";
+
+        content = Regex.Replace(content, actualContentRegex, contentToReplaceRegex);
+
+        File.WriteAllText(loaderPath, content);
+    }
+}
