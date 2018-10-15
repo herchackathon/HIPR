7e8b92b787bb654d50d49abd2a13f410dbb5a48c
Emanuele <m4nu.91@gmail.com>
2018-10-15 17:35:13 +0000

    Metamask/web3.js injection

---
+++ Assets/MHLab/Metamask/MetamaskManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MHLab.Metamask
+{
+    public class MetamaskManager
+    {
+        [DllImport("__Internal")]
+        public static extern string SendTransaction(string to, string data);
+
+        [DllImport("__Internal")]
+        public static extern string GetAccount();
+    }
+}
