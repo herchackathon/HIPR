e2927e8a87b450209974e5b31b7cc091722b60aa
Emanuele <m4nu.91@gmail.com>
2018-11-04 10:23:58 +0000

    New web build + Viktor's merged application

---
+++ WebBuild/public/javascripts/hipr-browser-internal.js
@@ -0,0 +1,312 @@
+HIPRInternal = {
+
+    isInit: false,
+
+    init () {
+		this.defaultWeb3()
+		
+		this.TestApi()
+    },
+
+    // restructured code class
+    defaultWeb3 () {
+        if (!this.isInit) {
+			this.isInit = true
+
+			// LINK to Web3Internal
+
+            Web3Internal.defaultWeb3()
+            this.playerScore = Web3Internal.playerScore
+            this.puzzleManager = Web3Internal.puzzleManager
+        }
+    },
+
+    requests: {
+		0: {
+			method: 'method name',
+			value: 'string|number'
+		}
+	},
+	requestId: 1,
+
+	getRequestId (name) {
+		var id = this.requestId++
+		this.requests[id] = {
+			name,
+			value: null,
+			error: null,
+			done: false
+		}
+		return id
+	},
+
+	setRequestValue (id, value) {
+		var req = this.requests[id]
+		if (!req) {
+			console.log(`request ${id} not found`)
+			return
+		}
+		if (req.value)
+			return
+		console.log('result', id, req.name, value)
+		req.value = value
+        req.done = true
+        
+        // LINK to HIPR Unity
+        SendResultBack(req.name, req.value)
+    },
+
+	setRequestError (id, error) {
+		var req = this.requests[id]
+		if (!req) {
+			console.log(`request ${id} not found`)
+			return
+		}
+		console.log('error', id, req.name, error)
+		req.error = error
+		req.done = true
+    },
+    
+    TestApi () {
+		var self = this
+		var rid = self.SetScore(789)
+		var puzzleId
+		setInterval(()=>{
+
+			var req = self.GetRequest(rid)
+			if (req) {
+				console.log(req)
+				if (rid == 1) {
+					rid = self.GetTopScores(10)
+				}
+				else if (rid == 2) {
+					rid = self.CreatePuzzle('puzzle 1a')
+				}
+				else if (rid == 3) {
+					rid = self.CreatePuzzle('puzzle 2b')
+				}
+				else if (rid == 4) {
+					puzzleId = req
+					rid = self.PushMetrics(puzzleId, 'puzzle 1')
+				}
+				else if (rid == 5) {
+					rid = self.CompareMetrics(puzzleId)
+				}
+				else if (rid == 6) {
+					debugger
+					rid = self.ValidatePuzzleResult(puzzleId, 'puzzle 2b')
+				}
+			}
+
+		}, 100)
+	},
+
+    /// <summary>
+	/// Get request for id
+    /// <param name="requestId">Request id returned by previous function call</param>
+    /// <returns>A string array with value or null if error</returns>
+	/// <summary>
+	GetRequest (requestId) {
+		var req = this.requests[requestId]
+		if (req && req.done) {
+			if (req.error) {
+				return null
+			}
+
+			var s = `${req.value}`
+
+            return s
+		}
+		else
+			return null
+	},
+
+	/// <summary>
+    /// Get top scores.
+    /// </summary>
+    /// <param name="count">The amount of top scores to retrieve.</param>
+    /// <returns>Request id -> string with results</returns>
+    GetTopScores: function(count)
+    {
+		this.defaultWeb3();
+
+		var self = this,
+			requestId = this.getRequestId('GetTopScores')
+
+		this.playerScore.GetTopScoresCount(function (error, result) {
+			if (!error) {
+				var values = new Array(count),
+					resultsCount = 0
+
+				for (var i = 0; i < count; i++) {
+					function f(i) {
+						self.playerScore.TopScores(i, function (error, result) {
+							var value = 0
+							if (!error)
+								value = `[${result[0]}, ${result[1].c[0]}]`
+							values[i] = {error, value}
+							if (++resultsCount == count) {
+								var s = ''
+								for (var j = 0; j < count; j++) {
+									if (values[j].error) {
+										self.setRequestError(requestId, error)
+										return
+									}
+									if (j != 0)
+										s += ', '
+									s += `${values[j].value}`
+								}
+								self.setRequestValue(requestId, s)
+							}
+						})
+					}
+					f(i)
+				}
+			}
+			else {
+				this.setRequestError(requestId, error)
+			}
+		})
+
+		return requestId
+	},
+
+    /// <summary>
+    /// Set the score for this player.
+    /// </summary>
+    /// <param name="score">The score to set.</param>
+    /// <returns>Request id -> true if success</returns>
+    SetScore: function(score)
+    {
+		this.defaultWeb3();
+
+		var self = this,
+			requestId = this.getRequestId('SetScore')
+
+		this.playerScore.SetScore(score, (error, result) => {
+			if (!error) {
+				self.setRequestValue(requestId, result)
+			}
+			else {
+				this.setRequestError(requestId, error)
+			}
+		})
+
+		return requestId
+    },
+
+	/// <summary>
+    /// Creates a new puzzle with given metrics.
+    /// </summary>
+    /// <param name="metrics">Metrics for new puzzle</param>
+    /// <returns>Request id -> puzzleId</returns>
+    CreatePuzzle (metrics)
+    {
+		this.defaultWeb3();
+
+		var self = this,
+			requestId = this.getRequestId('CreatePuzzle')
+
+		this.puzzleManager.CreatePuzzle(metrics, `${requestId}`, (error, result) => {
+			if (!error) {
+				console.log(`Contract created for reqId=${requestId} tx=${result}, waitig for PuzzleCreated event...`)
+			}
+			else {
+				this.setRequestError(requestId, error)
+			}
+		})
+
+		return requestId
+	},
+
+    /// <summary>
+    /// Pushes metrics for the given puzzle.
+    /// </summary>
+    /// <param name="puzzleId">Puzzle id</param>
+    /// <param name="metrics">Metrics for push</param>
+    /// <returns>Request id -> true if success</returns>
+	PushMetrics (puzzleId, metrics) 
+	{
+		this.defaultWeb3();
+
+		var self = this,
+			requestId = this.getRequestId('PushMetrics')
+
+		this.puzzleManager.PushMetrics(puzzleId, metrics, (error, result) => {
+			if (!error) {
+				self.setRequestValue(requestId, true)
+			}
+			else {
+				this.setRequestError(requestId, error)
+			}
+		})
+
+		return requestId
+	},
+
+	/// <summary>
+    /// Pushes metrics for the given puzzle.
+    /// </summary>
+    /// <param name="puzzleId">Puzzle id</param>
+    /// <returns>Request id -> true or false</returns>
+	CompareMetrics (puzzleId) 
+	{
+		this.defaultWeb3();
+
+		var self = this,
+			requestId = this.getRequestId('CompareMetrics')
+
+		this.puzzleManager.CompareMetrics(puzzleId, (error, result) => {
+			if (!error) {
+				self.setRequestValue(requestId, result)
+			}
+			else {
+				this.setRequestError(requestId, error)
+			}
+		})
+
+		return requestId
+	},
+
+    /// <summary>
+    /// Retrieves a puzzle hash from the blockchain.
+    /// </summary>
+    /// <returns>The metrics hash to encode.</returns>
+    GetPuzzle: function()
+    {
+
+    },
+
+
+    /// <summary>
+	/// Pushes the puzzle result to the smart contract for validation.
+	/// </summary>
+    /// <param name="puzzleId">Puzzle id</param>
+	/// <param name="resultHash">The resulting hash from the puzzle solving.</param>
+	/// <returns>Result id -> true if correctly validated, false if not.</returns>
+	ValidatePuzzleResult: function(puzzleId, resultHash)
+	{
+		this.defaultWeb3();
+
+		var self = this,
+			requestId = this.getRequestId('ValidatePuzzleResult')
+
+		this.puzzleManager.PushMetrics(puzzleId, resultHash, (error, result) => {
+			if (!error) {
+				this.puzzleManager.CompareMetrics(puzzleId, (error, result) => {
+					if (!error) {
+						self.setRequestValue(requestId, result)
+					}
+					else {
+						this.setRequestError(requestId, error)
+					}
+				})
+			}
+			else {
+				this.setRequestError(requestId, error)
+			}
+		})
+
+		return requestId
+	},
+}
---
+++ WebBuild/public/javascripts/web3-client-internal.js
@@ -0,0 +1,89 @@
+Web3Internal = {
+    // web3 [
+    
+        options: {
+            contracts: {
+                'PlayerScore': {
+                    abi: [{"constant":false,"inputs":[{"name":"score","type":"int256"}],"name":"SetScore","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"TopScores","outputs":[{"name":"player","type":"address"},{"name":"score","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"Scores","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"GetTopScoresCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}],
+                    net: 'ropsten',
+                    address: '0xA07B1FE246D9020f6884eA9d432B551Ea534b13f',
+                },
+                'PuzzleManager': {
+                    abi: [{"constant":false,"inputs":[],"name":"acceptPuzzle","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"puzzleId","type":"uint256"}],"name":"GetPuzzleMetrics","outputs":[{"name":"","type":"bytes"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"puzzleId","type":"uint256"}],"name":"CompareMetrics","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"puzzleId","type":"uint256"}],"name":"GetPuzzleOriginalMetrics","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"metrics","type":"string"},{"name":"uniqueId","type":"string"}],"name":"CreatePuzzle","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"puzzleId","type":"uint256"},{"name":"metrics","type":"string"}],"name":"PushMetrics","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"puzzleId","type":"uint256"},{"indexed":false,"name":"uniqueId","type":"string"}],"name":"PuzzleCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}],
+                    net: 'ropsten',
+                    address: '0xf792c43f23c39f7de185cfdc6ce96aa69e9f00c1',
+                }
+            }
+        },
+        
+        
+        _web3: {},
+    
+        getWeb3 (url) {
+            let web3 = this._web3[url]
+            if (web3 == undefined) {
+                if (url == 'metamask')
+                    web3 = window.web3
+    //			else
+    //				web3 = new Web3(new Web3.providers.HttpProvider(url))
+                this._web3[url] = web3
+                this.initWeb3(web3, this.options)
+            }
+            return web3
+        },
+    
+        defaultWeb3 () {
+            return this.getWeb3('metamask')
+        },
+    
+        initWeb3 (web3, options) {
+            //    web3.eth.subscribe('an_event', (error, event) => {})
+            this.playerScore = this.contractInit(web3, options.contracts['PlayerScore'])
+            this.puzzleManager = this.contractInit(web3, options.contracts['PuzzleManager'])
+    
+            // start events timer
+            
+            var self = this
+    
+            var fromBlock = 0
+    
+            setInterval(()=>{
+                self.puzzleManager.PuzzleCreated({}, (error, event)=>{
+                    fromBlock = event.blockNumber
+                    var puzzleId = event.args.puzzleId.c[0]
+                    var requestId = parseInt(event.args.uniqueId)
+                    
+//                    console.log(`Received PuzzleCreated from block ${fromBlock} puzzleId=${puzzleId} requestId=${requestId}`)
+
+                    // LINK to HIPRInternal
+
+                    HIPRInternal.setRequestValue(requestId, puzzleId)
+                })
+            }, 1000)
+        },
+    
+        contractInit (web3, options) {
+            let contract = web3.eth.contract(options.abi).at(options.address)
+            
+            if (contract)
+                console.log('new contract success at address:', options.address)
+    
+            return contract
+        },
+    
+        contractGetPastEvents (contract, eventName, options, cb) {
+            console.log(contract)
+            contract.getPastEvents(eventName, options, (error, results) => {
+                if (error) {
+                    console.log(error)
+                    return
+                }
+                results.forEach(event => {
+                    cb(event)
+                })
+            })
+        },
+    
+        // web3 ]
+    }
+    
