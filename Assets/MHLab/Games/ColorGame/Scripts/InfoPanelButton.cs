cbe3428547b7cbf08bdf49807fff256d12813e77
aaronsantamaria <a-santamaria@live.com>
2019-06-19 16:57:47 -0500

    Added an Info Panel to 5by5 Color Puzzle
    Added an info panel to toggle on/off while mid game.

---
+++ Assets/MHLab/Games/ColorGame/Scripts/InfoPanelButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanelButton : MonoBehaviour
+{
+
+    static int temp = 0;
+
+    public GameObject infoPanel;
+
+    public void SetInfoPanel()
+    {
+        if((infoPanel != null) && (temp == 0))
+        {
+            infoPanel.active = true;
+            temp = 1;
+        }
+        else
+        {
+            infoPanel.active = false;
+            temp = 0;
+        }
+    }
+
+
+
+}
